f37c299eb0bffa0e585f120974082cfec6ee6d53
Marko van Treeck <markovantreeck@gmail.com>
2025-01-06 14:47:45 +0100

    Update setup.py in concordance with huggingface page

--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@ from codecs import open
 from os import path
 
 setup(
-    name='uni',
-    version='0.1.0',
-    description='UNI',
-    url='https://github.com/mahmoodlab/UNI',
-    author='RJC,MYL,TD',
-    author_email='',
-    license='CC BY-NC 4.0',
-    packages=find_packages(exclude=['__dep__', 'assets']),
-    install_requires=["torch>=2.0.1", "torchvision", "timm==0.9.8", 
-                      "numpy", "pandas", "scikit-learn", "tqdm",
-                      "transformers"],
-
-    classifiers = [
-    "Programming Language :: Python :: 3",
-    "License :: CC BY-NC 4.0",
-]
+    name="uni",
+    version="0.1.0",
+    description="UNI",
+    url="https://github.com/mahmoodlab/UNI",
+    author="RJC,MYL,TD",
+    author_email="",
+    license="CC BY-NC 4.0",
+    packages=find_packages(exclude=["__dep__", "assets"]),
+    install_requires=[
+        "torch>=2.0.1",
+        "torchvision",
+        "timm>=0.9.8",
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "tqdm",
+        "transformers",
+        "xformers>=0.0.18",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC BY-NC 4.0",
+    ],
 )
